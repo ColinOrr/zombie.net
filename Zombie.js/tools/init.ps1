a8e75b7f0fc435d8c0de4dcc13441c4a3c6c7a2e
Colin Orr <colin.orr@gmail.com>
2016-01-28 21:11:37 +0000

    Fixed up the init script

--- Zombie.js/tools/init.ps1
+++ Zombie.js/tools/init.ps1
@@ -1,1 +1,3 @@
-Move-Item -Force -Path "$toolsPath\zombie" -Destination "$installPath..\..\node_modules"
+param($installPath, $toolsPath, $package, $project)
+New-Item -Force -ItemType directory "$installPath..\..\..\node_modules"
+Move-Item -Force -Path "$toolsPath\zombie" -Destination "$installPath..\..\..\node_modules\zombie"
