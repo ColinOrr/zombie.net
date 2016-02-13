fbc1d1cf477254fb7924fd2fd68e9f8d2c201298
Colin Orr <colin.orr@gmail.com>
2016-02-13 08:48:40 +0000

    Packaging zombie by itself didn't allow debugging, so including all the modules in node_modules. Fixes issue #2.

--- Zombie.js/tools/init.ps1
+++ Zombie.js/tools/init.ps1
@@ -1,3 +1,2 @@
 param($installPath, $toolsPath, $package, $project)
-New-Item -Force -ItemType directory "$installPath..\..\..\node_modules"
-Move-Item -Force -Path "$toolsPath\zombie" -Destination "$installPath..\..\..\node_modules\zombie"
+Move-Item -Force -Path "$toolsPath\node_modules" -Destination "$installPath..\..\..\node_modules"
