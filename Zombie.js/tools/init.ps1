f28d89558e06c03aba5a61cf6e90f53054b08cff
Colin Orr <colin.orr@gmail.com>
2016-02-15 20:20:40 +0000

    Now installing zombie.js as an after build step to allow it to work from a NuGet package restore. Fixes #1.

--- Zombie.js/tools/init.ps1
+++
@@ -1,3 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-New-Item -Force -ItemType directory "$installPath..\..\..\node_modules"
-Move-Item -Force -Path "$toolsPath\zombie" -Destination "$installPath..\..\..\node_modules\zombie"
