2e0f2fa461c86a0a7c5f398d4fa0459ef245d737
Matt Teeney <matthew.teeney@civica.co.uk>
2016-04-06 14:43:18 +0100

    Moving the process.on(...) handler to after the require('zombie') so that any failed efforts to initialise zombie doesn't result in re-registering the listener.

--- Zombie/ZombieDriver.js
+++ Zombie/ZombieDriver.js
@@ -1,9 +1,9 @@
 "use strict";
 
-process.on('uncaughtException', e => console.error('Uncaught Exception: ' + e));
-
 const Zombie = require('zombie');
 const zombie = new Zombie();
+
+process.on('uncaughtException', e => console.error('Uncaught Exception: ' + e));
 
 //  Entry point for Edge.js
 return function(data, callback) {
